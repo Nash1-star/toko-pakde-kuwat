9d18b144503b1942247971934e0a18039ceba015
Riza Aqilah Nashifah <rizaan2827@gmail.com>
2026-09-22 19:55:31 +0700

    Trust Railway proxy HTTPS headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias(['check.shift' => \App\Http\Middleware\CheckShift::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
