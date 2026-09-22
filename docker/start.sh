d84f03558abc12a6af14ac1ae0243a36abd346ee
Riza Aqilah Nashifah <rizaan2827@gmail.com>
2026-09-22 19:28:03 +0700

    Make Railway startup database resilient

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+if [ -z "${DB_HOST:-}" ] && [ -z "${DB_URL:-}" ]; then
+	export DB_CONNECTION=sqlite
+	export DB_DATABASE=/var/www/html/database/database.sqlite
+	export SESSION_DRIVER=file
+	export CACHE_STORE=file
+	export QUEUE_CONNECTION=sync
+	mkdir -p database
+	touch "$DB_DATABASE"
+fi
+
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
